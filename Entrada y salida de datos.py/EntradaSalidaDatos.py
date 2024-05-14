9479c79b6707da4ccc0c8ae6a7ca842a67919bf0
AxelSandoval04 <ajaviersandoval4@gmail.com>
2024-05-14 09:34:34 -0600

    Poo

---
+++ Entrada y salida de datos.py/EntradaSalidaDatos.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+# Entrada de datos
+nombre=input("Nombre del alumno:")
+matricula=input("Matricula:")
+especialidad=input("Especialidad:")
+edad=input("Edad:")
+estatura=input("Estatura:")
+
+print(f" ... Datos del alumno ::: ...  \n Nombre: {nombre} \n Matricula: {matricula} \n Edad: {edad} \n Especialidad: {especialidad} \n ")
---
+++ Entrada y salida de datos.py/if.py
@@ -0,0 +1,33 @@
+"""
+Existe una estructura de condicion la cual evalua una condicion para encontrar el valor TRUE o se cumple una condicion ejecuta la linea
+o lineas de codigo
+"""
+
+# 1.- if simple 
+# 2.- if compuesto
+# 3.- if anidado
+# 4.- if y elseif
+
+# 1.+
+color="rojo"
+if color =="rojo":
+    print("Soy el color rojo")
+# 2-
+if color =="rojo":
+    print("Soy el color rojo")
+else:
+    print("No soy rojo")
+# 3.-
+if color =="rojo":
+    print("Soy el color rojo")
+    if color !="rojo":
+      print("Soy otro rojo")
+else: print("No soy rojo")
+# 4.-
+if color =="rojo":
+    print("Soy el color rojo")
+elif color =="negro":
+    print("Soy el color Verde")
+else:
+    print("No soy ni Rojo ni Verde")
+
